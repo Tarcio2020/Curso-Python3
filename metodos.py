679cf6f64592735af6f6bcb3d54cfde2931987e1
Tarcio Teles <tarcioapps7@gmail.com>
2023-07-12 18:32:58 -0300

    add primeiros arquivos

---
+++ metodos.py
@@ -0,0 +1,42 @@
+print('Hello Word!!!')
+
+def meu_metodo():
+    print('olá')
+    print('mundo')
+meu_metodo()
+
+
+
+print('do lado de fora')
+
+
+def soma_De_Dois_Valores(valor1,valor2):
+    return valor1+valor2
+
+print(soma_De_Dois_Valores(50,50))
+
+
+### Built in -Imnutidas
+
+print(int(10.3))
+
+print(float(10))
+
+print(len('olá'))#tamanho
+
+
+x=-10
+print(abs(x)) #modulo
+
+lista = [1,2,3,4,5,6,7,8,9]
+
+print(sum(lista))
+print(min(lista))
+print(max(lista))
+
+
+
+print(round(5.489849415151))
+
+
+
