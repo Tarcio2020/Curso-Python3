a4c244b2f4194a06d7666b38d0bd095f81056c8e
Tarcio Teles <tarcioapps7@gmail.com>
2023-07-12 18:11:31 -0300

    add primeiros arquivos

---
+++ aula_Um.py
@@ -0,0 +1,25 @@
+#Início##
+
+inicio = 'Hello Word'
+
+print(inicio)
+a = 2
+b = 3
+c='15'
+e,f,g = 1 ,2, 3
+h ='A letra "b" está com aspas'
+
+resultado = a **b
+
+print(resultado)
+print(type(a))
+print(type(b))
+print(type(c))
+print(c*2)
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+
+
---
+++ operadores_Logicos.py
@@ -0,0 +1,24 @@
+#
+
+a = 3+2
+b = 2-3
+c= 10/3
+d = 10*3
+e = 10%3
+
+f = 3**2
+g = 2>12
+h = 2<=2
+i = 2>=8
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(d)
+print(e)
+print(f)
+print(g)
+print(h)
+print(i)
+
+A
